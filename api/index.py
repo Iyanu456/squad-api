4260dc6e2c53dc9bbb17bbf377527df855dc7f44
Iyanu456 <oyerindei13@gmail.com>
2023-12-28 22:08:03 +0100

    implementing squid api

--- api/index.py
+++ api/index.py
@@ -5,12 +5,27 @@ from wtforms.validators import DataRequired, Email
 from flask_wtf.csrf import CSRFProtect
 from mongoengine import NotUniqueError
 from Models.models import User, StorageEngine
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 storage_engine = StorageEngine('squidDB', 'localhost', 27017)
 
 csrf = CSRFProtect(app)
+
+# Function to make a POST request to the endpoint
+def make_post_request(endpoint, user_data):
+
+    authorization_token = "sandbox_pk_25c04c9060872f1e36b8310d615e87da36aa3c51055f"
+
+    headers = {
+        "Authorization": f"Bearer {authorization_token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(endpoint, json=user_data, headers=headers)
+
+    return response
 
 # Define the WTForms form
 class UserForm(FlaskForm):
@@ -40,6 +55,22 @@ def index():
             # Save the form data to the database
             new_user = User(username=username, first_name=first_name, last_name=last_name, middle_name=middle_name, email=email, password=password,)
             new_user.save()
+
+            # Make a POST request to the endpoint
+            user_data = {
+                "display_name": first_name + last_name,
+                "account_name": first_name + middle_name + last_name,
+            }
+
+            print(user_data)
+            response = make_post_request("https://sandbox-api-d.squadco.com/merchant/create-sub-users", user_data)
+    
+            # Check the response and handle accordingly
+            if response.status_code == 200:
+                print(response)
+            else:
+                print(response)
+
             return redirect(url_for('success'))
         except NotUniqueError as e:
             # Handle the unique constraint violation
