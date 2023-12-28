8e4c9659775ca43b1ef38eafd8fc364f9b4fdac7
Iyanu456 <oyerindei13@gmail.com>
2023-12-28 21:23:59 +0100

    working on database

---
+++ api/Models/models.py
@@ -0,0 +1,65 @@
+from mongoengine import connect
+
+from mongoengine import Document, StringField, EmailField
+
+class User(Document):
+    """
+    MongoDB Document Model for User
+    """
+    username = StringField(required=True, unique=True)
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True)
+
+    meta = {'collection': 'users'}
+
+    def __str__(self):
+        return f"User(username={self.username}, email={self.email})"
+
+
+class StorageEngine:
+    def __init__(self, db_name, host='localhost', port=27017):
+        connect(db=db_name, host=host, port=port)
+
+    def save_user(self, username, email, password):
+        """
+        Save a new user to the MongoDB database.
+        """
+        new_user = User(username=username, email=email, password=password)
+        new_user.save()
+        print(f"User {username} saved successfully.")
+
+    def delete_user(self, username):
+        """
+        Delete a user from the MongoDB database.
+        """
+        deleted_user = User.objects(username=username).first()
+        if deleted_user:
+            deleted_user.delete()
+            print(f"User {username} deleted successfully.")
+        else:
+            print(f"User {username} not found.")
+
+    def find_user(self, email):
+        """
+        Find a user based on their email.
+        """
+        user = User.objects(email=email).first()
+        if user:
+            print(f"User found: {user}")
+        else:
+            print(f"User with email {email} not found.")
+
+if __name__ == "__main__":
+    # Assuming User is already defined as shown in the previous examples
+
+    # Create a StorageEngine instance and connect to the database
+    storage_engine = StorageEngine(db_name='your_database', host='your_mongo_host', port=your_mongo_port)
+
+    # Save a new user
+    storage_engine.save_user(username='john_doe', email='john@example.com', password='hashed_password')
+
+    # Find a user by email
+    storage_engine.find_user(email='john@example.com')
+
+    # Delete a user
+    storage_engine.delete_user(username='john_doe')
--- api/index.py
+++ api/index.py
@@ -1,11 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_wtf.csrf import CSRFProtect
+from mongoengine import NotUniqueError
+from Models.models import User, StorageEngine
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+storage_engine = StorageEngine('squidDB', 'localhost', 27017)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+csrf = CSRFProtect(app)
+
+# Define the WTForms form
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Route for the form
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        try:
+            # Save the form data to the database
+            new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+            new_user.save()
+            return redirect(url_for('success'))
+        except NotUniqueError as e:
+            # Handle the unique constraint violation
+            print(f"Error: {e}", 'error')
+
+    return render_template('index.html', form=form)
+
+# Route for a success page
+@app.route('/success')
+def success():
+    return 'Form submitted successfully!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == "__main__":
+    # Enable debug mode
+    app.run(debug=True)
